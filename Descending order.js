3b88dab7d062a91fbd03c5909408972de117a1b1
Arun20072002 <144424842+Arun20072002@users.noreply.github.com>
2023-11-07 10:17:21 +0530

    Update Descending order.js

--- Descending order.js
+++ Descending order.js
@@ -3,25 +3,25 @@ document.write("--------------------------------------"+"<br>")
 var a=5,b=500,c=50;
 if((a>b)&& (a>c)){
     if(b>c){
-        document.write("a,b,c"+"<br>")
+        document.write(a,b,c+"<br>")
     }
     else{
-        document.write("a,c,b"+"<br>")
+        document.write(a,c,b+"<br>")
     }
 }
 else if((b>a)&& (b>c)){
 if(a>c){
-    document.write("b,a,c"+"<br>")
+    document.write(b,a,c+"<br>")
 }
 else{
-    document.write("b,c,a"+"<br>")
+    document.write(b,c,a+"<br>")
 }
 }
 else if((c>a) && (c>b)){
 if(b>a){
-document.write("c,b,a"+"<br>")
+document.write(c,b,a+"<br>")
 }
 else{
-document.write("c,a,b"+"<br>")
+document.write(c,a,b+"<br>")
 }
 }
